3702060e7c3a8a5eac6336dc3d74177ecbd484e0
JelenaDjelic <djelicjeka@gmail.com>
2026-02-01 21:54:19 +0100

    /

--- frontend/src/Components/home/CircleButtons.tsx
+++ frontend/src/Components/home/CircleButtons.tsx
@@ -5,11 +5,8 @@ const CircleButtons: React.FC = () => (
   <Slider>
     
     <ButtonCircle icon='stadium' text='Concert' url='list' />
-    
     <ButtonCircle icon='sports' text='Sports' url='list' />
-    
     <ButtonCircle icon='piano' text='Musical' url='list' />
-    
     <ButtonCircle icon='festival' text='Festival' url='list' />
   </Slider>
 );
